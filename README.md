8eaddcd6c2abfca938c6996b7e73e309d8752285
Lachbok <lachbok12@gmail.com>
2025-06-09 13:22:45 +0800

    many changes
    added a binary sort by timestamp to the displayed posts. added instructions to README.md on how to make a post. and made a 3rd post. also some changes to the css in regards to wrapping

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Lachbok.github.io
+To make a post:
+ 1. in the main folder, open post.txt
+ 2. the first line put the title of your post
+ 3. the following lines put the content/text of your post
+ 4. save the file
+ 5. run the makepost.py script
+ 6. [optional] open the index.html file in your browser to check if everything worked correctly
+ 7. push the changes to the GITHUB repository(may take a few minutes to actually show up)
+
+How to run the makepost.py script:
+First, open command prompt in the main folder(where makepost.py is located). 
+Can do this by clicking on the file path in the folder window(the long thin rectangular strip above the folder contents), the file path will appear highlighted, delete this and type 'cmd' then press enter.
+In the command prompt write 'python makepost.py' then press enter.
+This will run the script and it will be done.
--- makepost.py
+++ makepost.py
@@ -13,7 +13,7 @@ now = datetime.now()
 formatted = now.strftime("%Y-%m-%d %H:%M:%S")
 if posting:
     post = f"<div class='postcontainer'>\n<p class='posttime'>{formatted}</p><p class='posttitle'>{title}</p>\n<p class='posttext'>{text}\n</p>\n</div>\n"
-    with open(f"posts/{title}-{formatted.replace(':','-')}.txt".replace(' ','_'), "w", encoding="utf-8") as save:
+    with open(f"posts/{title}_{formatted.replace(':','-')}.txt".replace(' ','_'), "w", encoding="utf-8") as save:
         save.write(post)
 
 #delete post.txt
@@ -21,10 +21,53 @@ with open("post.txt", "w", encoding="utf-8") as file:
     file.write('')
     
 #loop through all posts and compile string
-all_posts = ''
 folder_path = "posts"
+sorted_posts = [] #descending order (newest first)
 for filename in os.listdir(folder_path):
-    full_path = os.path.join(folder_path, filename)
+    timestamp = ''.join(filename.split('_')[-2:]).replace('-','') #["YEAR-MONTH-DAY","HOUR-MINUTE-SECOND"] -> "YEARMONTHDAYHOURMINUTESECOND"
+    if len(sorted_posts)==0:
+        sorted_posts.append([timestamp,filename])
+        continue
+    else: #binary search and place at index
+        length = len(sorted_posts)
+        upper = length-1
+        lower = 0
+        while True:
+            mid = (upper+lower)//2
+            midts = sorted_posts[mid][0]
+            if timestamp>midts:
+                if mid+1<length and timestamp<sorted_posts[mid+1][0]:
+                    #put at index mid+1
+                    sorted_posts.insert(mid+1,[timestamp,filename])
+                    break
+                elif not mid+1<length: #aka mid==len-1
+                    #put as very last element
+                    sorted_posts.append([timestamp,filename])
+                    break
+                else:
+                    lower = mid+1
+                    continue
+            elif timestamp<midts:
+                if mid-1>-1 and timestamp>sorted_posts[mid-1][0]:
+                    #put at index mid-1
+                    sorted_posts.insert(mid-1,[timestamp,filename])
+                    break
+                elif not mid-1>-1: #aka mid==0
+                    #put as very first element
+                    sorted_posts.insert(0,[timestamp,filename])
+                    break
+                else:
+                    upper = mid
+                    continue
+            else:
+                print("Two posts have same timestamp, probably duplicates.")
+                continue
+
+#compile sorted posts into a string
+sorted_posts.reverse()
+all_posts = ''
+for post in sorted_posts:
+    full_path = os.path.join(folder_path, post[1])
     with open(full_path, "r", encoding="utf-8") as file:
         all_posts += file.read()
 
@@ -32,9 +75,7 @@ for filename in os.listdir(folder_path):
 with open("baseindex.html", "r", encoding="utf-8") as file:
     content = file.read()
 
-#pos = len(content)-19
-#new = '\n'+content[:pos]+post+content[pos:]+'\n'
-new = content.replace('###',all_posts)
+new = content.replace('$POSTHERE$',all_posts)
 with open("index.html", "w", encoding="utf-8") as file:
     file.write(new)
 
